645361039fc46e053c2775abb51d44f1195ec6aa
flobri <christoph123.alpen@googlemail.com>
2024-06-07 10:12:14 +0200

    Update main.py

--- valueinv/main.py
+++ valueinv/main.py
@@ -62,7 +62,7 @@ with st.container():
         if ticker:
             try:
                 description = overview['Description'].iloc[0]
-                name = overview['Name'].to_list().replace(' ', '+')
+                name = overview['Name'].iloc[0].replace(' ', '+')
                 sec_url = f"https://www.sec.gov/cgi-bin/browse-edgar?company={name}&match=starts-with&filenum=&State=&Country=&SIC=&myowner=exclude&action=getcompany"
 
                 
